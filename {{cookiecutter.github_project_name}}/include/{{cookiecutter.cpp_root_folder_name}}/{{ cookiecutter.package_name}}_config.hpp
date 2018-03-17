6a44c597c975f49a0ad25dd2402e53b96781d077
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2018-03-17 17:52:05 +0100

    fixed wrong lower cases in cofig.hpp

--- {{cookiecutter.github_project_name}}/include/{{cookiecutter.cpp_root_folder_name}}/{{ cookiecutter.package_name}}_config.hpp
+++ {{cookiecutter.github_project_name}}/include/{{cookiecutter.cpp_root_folder_name}}/{{ cookiecutter.package_name}}_config.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef {{ cookiecutter.cpp_macro_prefix }}_{{cookiecutter.package_name|upper}}_config_HPP
-#define {{ cookiecutter.cpp_macro_prefix }}_{{cookiecutter.package_name|upper}}_config_HPP
+#ifndef {{ cookiecutter.cpp_macro_prefix }}_{{cookiecutter.package_name|upper}}_CONFIG_HPP
+#define {{ cookiecutter.cpp_macro_prefix }}_{{cookiecutter.package_name|upper}}_CONFIG_HPP
 
 
 
@@ -14,4 +14,4 @@ namespace {{ cookiecutter.cpp_namespace }} {
 } // end namespace {{cookiecutter.cpp_namespace}}
 
 
-#endif // {{ cookiecutter.cpp_macro_prefix }}_{{cookiecutter.package_name|upper}}_config_HPP
+#endif // {{ cookiecutter.cpp_macro_prefix }}_{{cookiecutter.package_name|upper}}_CONFIG_HPP
