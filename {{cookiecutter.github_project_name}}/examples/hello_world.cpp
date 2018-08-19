a13a11a54e911dd465eb5277eb4deb09a783bfe7
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2018-08-19 20:43:07 +0200

    chaged stuff

--- {{cookiecutter.github_project_name}}/examples/hello_world.cpp
+++ {{cookiecutter.github_project_name}}/examples/hello_world.cpp
@@ -1,6 +1,8 @@
 #include "{{cookiecutter.cpp_root_folder_name}}/{{cookiecutter.package_name}}.hpp"
 #include "{{cookiecutter.cpp_root_folder_name}}/{{cookiecutter.package_name}}_config.hpp"
 
+#include <xtensor/xarray.hpp>
+
 int main(int argc, char *argv[]){
 
 }
