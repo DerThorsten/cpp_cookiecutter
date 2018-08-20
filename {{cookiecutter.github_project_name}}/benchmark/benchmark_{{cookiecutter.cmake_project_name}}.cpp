91bc3350e8e2233a6f6754b80125492d7a1b0dcc
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2018-08-20 09:50:24 +0200

    changed bm

--- {{cookiecutter.github_project_name}}/benchmark/benchmark_{{cookiecutter.cmake_project_name}}.cpp
+++ {{cookiecutter.github_project_name}}/benchmark/benchmark_{{cookiecutter.cmake_project_name}}.cpp
@@ -1,6 +1,13 @@
 #include <benchmark/benchmark.h>
 
 #include <string>
+#include <vector>
+
+#include "xtensor/xmath.hpp"
+#include "xtensor/xarray.hpp"
+
+// our headers
+#include "{{cookiecutter.cpp_root_folder_name}}/{{ cookiecutter.package_name}}.hpp"
 
 
 static void BM_SomeBenchmark(benchmark::State& state)
@@ -14,3 +21,22 @@ static void BM_SomeBenchmark(benchmark::State& state)
 }
 // Register the function as a benchmark
 BENCHMARK(BM_SomeBenchmark);
+
+
+
+
+
+static void BM_SomeBenchmark2(benchmark::State& state)
+{
+    std::size_t s = state.range(0);
+
+    while (state.KeepRunning())
+    {
+        benchmark::DoNotOptimize(
+            std::vector<int>(s)
+        );
+    }
+}
+// Register the function as a benchmark
+// Now arguments generated are [ 8, 16, 32, 64, 128, 256, 512, 1024, 2k, 4k, 8k ].
+BENCHMARK(BM_SomeBenchmark2)->RangeMultiplier(2)->Range(8, 8<<12);
