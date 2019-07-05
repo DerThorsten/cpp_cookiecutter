4379947c37e4c85064fc2029d4f6fd78adcdb539
DerThorsten <derthorstenbeier@gmail.com>
2019-07-05 13:16:23 +0200

    added files

---
+++ setup.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='cpp_cookiecutter',
+    packages=[],
+    version='0.2.0',
+    description='Cookiecutter template for a C++ package',
+    author='Thorsten Beier',
+    license='BSD 3-Clause License',
+    author_email='derthorstenbeier@gmail.com',
+    url='https://github.com/DerThorsten/cpp_cookiecutter',
+    keywords=['cookiecutter', 'template', 'package', ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD 3-Clause License',
+        'Programming Language :: C',
+        'Programming Language :: C++ :: 2.7',
+        'Programming Language :: C++ :: 3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+    ],
+)
