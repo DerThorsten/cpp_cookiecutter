2a17f29b21a22966a179f9e408e786eea03342e6
DerThorsten <derthorstenbeier@gmail.com>
2019-07-08 18:15:58 +0200

    also build docs in unit tests

--- tests/test_project.sh
+++ tests/test_project.sh
@@ -7,55 +7,95 @@ SOURCE=$2
 echo $TEMPLATE_FOLDER
 echo $SOURCE
 
+echo "------------------------------------------------------------"
 echo "create cpp_cookiecutter-dev-requirements conda env"
+echo "------------------------------------------------------------"
 conda env create -f cpp_cookiecutter-dev-requirements.yml || exit 1
 
+echo "------------------------------------------------------------"
 echo "activate cpp_cookiecutter-dev-requirements conda env"
+echo "------------------------------------------------------------"
 $SOURCE activate cpp_cookiecutter-dev-requirements || exit 1
 
+echo "------------------------------------------------------------"
 echo "take one step back"
+echo "------------------------------------------------------------"
 cd .. || exit 1
 
+echo "------------------------------------------------------------"
 echo "render coookiecutter template"
+echo "------------------------------------------------------------"
 cookiecutter $TEMPLATE_FOLDER --overwrite-if-exists --no-input || exit 1
 
+echo "------------------------------------------------------------"
 echo "go into cpp tools"
+echo "------------------------------------------------------------"
 cd cpptools || exit 1
 
+echo "------------------------------------------------------------"
 echo "create cpptools-dev-requirements conda env"
+echo "------------------------------------------------------------"
 conda env create -f cpptools-dev-requirements.yml || exit 1
 
+echo "------------------------------------------------------------"
 echo "activate cpptools-dev-requirements conda env"
+echo "------------------------------------------------------------"
 $SOURCE activate cpptools-dev-requirements || exit 1
 
+echo "------------------------------------------------------------"
 echo "create build dir"
+echo "------------------------------------------------------------"
 mkdir build || exit 1
 
+echo "------------------------------------------------------------"
 echo "go into build dir"
+echo "------------------------------------------------------------"
 cd build || exit 1
 
+echo "------------------------------------------------------------"
 echo "run cmake"
+echo "------------------------------------------------------------"
 cmake .. || exit 1
 
+echo "------------------------------------------------------------"
 echo "build project"
+echo "------------------------------------------------------------"
 make -j2 || exit 1
 
+echo "------------------------------------------------------------"
 echo "run cpp tests"
+echo "------------------------------------------------------------"
 make cpp-test || exit 1
 
+echo "------------------------------------------------------------"
 echo "run python tests"
+echo "------------------------------------------------------------"
 make python-test || exit 1
 
 
+echo "------------------------------------------------------------"
 echo "run example"
+echo "------------------------------------------------------------"
 cd examples || exit 1
 ./hello_world || exit 1
 cd .. || exit 1
 
+echo "------------------------------------------------------------"
 echo "run benchmark"
+echo "------------------------------------------------------------"
 cd benchmark || exit 1
 ./benchmark_cpptools || exit 1
 cd .. || exit 1
 
 
+echo "------------------------------------------------------------"
+echo "build docs"
+echo "------------------------------------------------------------"
+cd docs || exit 1
+make || exit 1
+cd .. || exit 1
+
+
+echo "------------------------------------------------------------"
 echo "Completed test script successfully"
+echo "------------------------------------------------------------"
